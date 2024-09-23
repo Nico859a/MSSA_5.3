ead4fbc0db08ffaf012d6f20e72c90d4585fb5b3
Nicholas Potenza <nicholas.potenza@outlook.com>
2024-09-23 16:13:04 -0500

    Add project files.

---
+++ Assignment5.3.2/Stairs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5._3._2
+{
+    internal class Stairs
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public int ClimbingStairs(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+                
+            if (n == 2)
+            {
+                return 2;
+            }
+
+            int first = 1;
+            int second = 2;
+            int result = 0;
+
+            for (int i = 3; i <= n; i++)
+            {
+                result = first + second;
+                first = second;
+                second = result;
+            }
+            return result;
+        }
+    }
+}
---
+++ Assignment5.3/Flowers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5._3
+{
+    internal class Flowers
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            Flowers([1, 0, 0, 0, 1], n);
+        }
+
+        static bool Flowers(int[] flowerbed, int n)
+        {
+            int count = 0;
+            for (int i = 0; i < flowerbed.Length; i++)
+            {
+                if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) &&
+                    (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0))
+                {
+                    flowerbed[i] = 1;
+                    count++;
+                }
+            }
+            return count >= n;
+        }
+    }
+}
